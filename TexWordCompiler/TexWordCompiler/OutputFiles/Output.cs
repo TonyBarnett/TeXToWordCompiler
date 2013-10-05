c47ef905ef37c7abe630109ad82dbdf57c183201
Tony <tony_barnett@hotmail.com>
2013-10-05 23:28:43 +0100

    the beginnings of interopping...

--- TexWordCompiler/TexWordCompiler/OutputFiles/Output.cs
+++ TexWordCompiler/TexWordCompiler/OutputFiles/Output.cs
@@ -10,9 +10,12 @@ namespace TexWordCompiler.OutputFiles
     {
         public Output()
         {
-            Application app = new Application();
-
-            app.NewWindow();
+            Document d = new Document(); // Create a document
+            Paragraph p = d.Content.Paragraphs.Add(); // Create a paragraph as addrd to the document.
+            p.Range.Text = "eragsat"; // Add text to the paragraph
+            string funky = d.WordOpenXML; // read out the xml, might be a better idea to read this out to a 
+                // streamWriter to form the word 2007 format
+            //d.Save(); // save thge document
         }
     }
 }
