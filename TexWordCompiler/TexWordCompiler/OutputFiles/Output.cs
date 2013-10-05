75e29a9a705bdb2937abf17fee9f2fc65fcaff33
Tony Barnett <tony.barnett@ukplc.net>
2013-10-05 19:35:24 +0100

    Change to use interop libraries

---
+++ TexWordCompiler/TexWordCompiler/OutputFiles/Output.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Word;
+
+namespace TexWordCompiler.OutputFiles
+{
+    class Output
+    {
+        public Output()
+        {
+            Application app = new Application();
+
+            app.NewWindow();
+        }
+    }
+}
